be95d1f7a2f36b2b8de406bb245621cab3329323
Pasrija <mohit.pasrija@intel.com>
2020-01-05 14:22:50 -0800

    first commit

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
-#include<studio.h>
+#include<stdio.h>
+#include<malloc.h>
 
 typedef struct node
 {
@@ -6,31 +7,82 @@ typedef struct node
     struct node* next;
 }node;
 
-void push(node* head, int data)
+void push(node** head, int data)
 {
     node* temp = (node*)malloc(sizeof(node));
     temp->data = data;
-    temp->next = head;
-    head = temp;
+    temp->next = *head;
+    *head = temp;
+}
+
+void insert_end(node** head, int data)
+{
+    node* temp = (node*)malloc(sizeof(node));
+    temp->data = data;
+    temp->next = NULL;
+    
+    if(*head == NULL)
+    {
+       temp->next = *head;
+        *head = temp;     
+        return;
+    }
+    node* temp2 = *head;
+    while(temp2->next!=NULL)
+    {
+        temp2 = temp2->next;   
+    }
+    temp2->next = temp;
+}
+
+void delete_at_beg(node** head)
+{
+    node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+void delete_at_end(node** head)
+{
+    node* temp = *head;
+    node* temp2 = NULL;
+    
+    while(temp->next)
+    {
+        temp2 = temp;
+        temp = temp->next;
+    }
+    
+    temp2->next = NULL;
+    free(temp);
 }
 
 void print(node* head)
 {
     node* temp = head;
-    while(temp->next!=NULL)
+    while(temp)
     {
-        print("%d\t",temp->data);
+        printf("%d\t",temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
 
 
 int main()
 {
     node* head = NULL;
-    push(head, 2);
-    push(head, 3);
-    push(head, 4);
-    print();
+    push(&head, 2);
+    insert_end(&head,6);
+    push(&head, 3);
+    insert_end(&head,7);
+    push(&head, 4);
+    insert_end(&head,5);
+    print(head);
+    delete_at_beg(&head);
+    print(head);
+    delete_at_end(&head);
+    print(head);
+    delete_at_beg(&head);
+    print(head);
 }
-
